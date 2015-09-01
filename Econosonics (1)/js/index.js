0f02a787d09acd67d2df568c7622f0420048776d
sayersp <patricksayers@gmail.com>
2015-08-31 21:25:14 -0400

    Initial Commit

---
+++ Econosonics (1)/js/index.js
@@ -0,0 +1,41 @@
+/*Amoeba 
+Who We Are
+Since 1990, Amoeba Music has welcomed independent music lovers of all kinds to our unique, vibrant stores in Berkeley, San Francisco and Hollywood.  We stock every kind of music and movies -- from the top 40 to the best in underground rock and hip-hop, soul, electronica, new and classic jazz, world music, roots music and experimental music. We also have the biggest, most diverse and affordable selection of DVDs and vinyl LPs found anywhere. Since we're a trading post for new and used discs, our stock changes daily, and just about anything you're looking for can be found here. We're more than just a record store -- we're a 21st century music outlet, a website, a popular live performance venue, and together with our customers we're a meeting place for California's most colorful community of progressive and creative minds.
+
+Our staff is an all-star team of music retail veterans, with a collective depth of knowledge that is virtually unparalleled in the business. Many of us are musicians, or make music our lives in one way or another, and we take seriously the importance of our customers' relationship to music. We put customer service first and foremost -- our mission is to bring people and music together and to make everyone feel at home.
+
+Amoeba Music began at a time when the huge chain stores were mercilessly swallowing up independent stores and local chain stores, depriving communities across the country of a personal relationship with their music outlets, and destroying the opportunity to discover a whole world of music beyond what corporate retailers wanted them to see.  Amoeba arose out of that community of music lovers that wanted a better place for music than a corporate chain store -- one with the widest possible selection, better service, and more respect for people's ideas and lives. Together with you, we are thatcommunity of independent artists and listeners that wanted something more, and with you we've been able to take root and thrive in California's most amazing cities. We're humbled by and thankful for the continuing support and success we've found in trying to create a store and a venue that's worthy of great music.
+
+Our San Francisco and Hollywood stores are big-city sized but have that same independent Berkeley spirit. These easy-to-get-to locations in the classic Haight-Ashbury and Sunset Strip locales feature vast, unbeatable selection, and are constantly hosting incredible free live performances and musical events. If you love music, let us be your ultimate source for inspiration!
+
++++++++++++++
+Penny Club (artist picks/ discussion)
++++++++++++++
+Blog
+News posts, videos, local events, sales, charity
+++++++++++++++
+
+
+Sergio Mendes/ Led Zep stacks (photo credit) Found on Photopin/Downloaded
+
+photo credit: <a href="http://www.flickr.com/photos/23829501@N00/3027510021"></a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by/2.0/">(license)</a>
+
+License: <a href="https://creativecommons.org/licenses/by/2.0/">(license)</a>
+
+******************
+CD  shelves (photo credit) Found on Photopin/Downloaded
+photo credit: <a href="http://www.flickr.com/photos/59836631@N06/5602080480">Frank, April 8, 2011 - CDs</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by/2.0/">(license)</a>
+
+License: <a href="https://creativecommons.org/licenses/by/2.0/">(license)</a>
+**********************
+LP playing (photo credit) Found on Photopin/Downloaded
+
+photo credit: <a href="http://www.flickr.com/photos/50245168@N00/5442830029">Rotel</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by/2.0/">(license)</a>
+
+License: <a href="https://creativecommons.org/licenses/by/2.0/">(license)</a>
+***********************
+
+
+
+
+*/
